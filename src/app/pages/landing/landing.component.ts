0a24f6f43decff59457dbbeb68b42683308deced
mahalisunil1 <106264269+mahalisunil1@users.noreply.github.com>
2024-08-02 19:42:18 +0530

    minor fixes

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -77,21 +77,19 @@ mobileLastOrientation:boolean = window.innerWidth < window.innerHeight
     }
 
     if (window.innerWidth < 576) {
-      console.log("test is working")
+      // console.log("test is working")
+      const currentOrientationMobile = window.innerWidth < window.innerHeight
+  
+      if (currentOrientationMobile !== this.mobileLastOrientation) {
+        // Update the last orientation to current
+        // Reload the page if the orientation has changed
+        // window.location.reload();
+          this.mobileLastOrientation = currentOrientationMobile;
+          console.log("orientation working")
+        this.cdr.detectChanges()
+      }
     }
 
-    // const currentOrientationMobile = window.innerWidth < window.innerHeight
-
-    // if (currentOrientationMobile !== this.mobileLastOrientation) {
-    //   // Update the last orientation to current
-    //   // Reload the page if the orientation has changed
-    //   // window.location.reload();
-    //   if (window.innerWidth < 576) {
-    //     this.mobileLastOrientation = currentOrientationMobile;
-    //     console.log("orientation working")
-    //   }
-    //   this.cdr.detectChanges()
-    // }
 
 
   }
